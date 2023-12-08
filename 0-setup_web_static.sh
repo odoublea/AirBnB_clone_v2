aea50a03afc0caf29aff9496921cd42678464497
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-12-08 17:21:13 +0100

    update setup script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,46 +1,42 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
 
-# Install Nginx if not already installed
-if ! dpkg -s nginx >/dev/null 2>&1; then
-  sudo apt-get update
-  sudo apt-get install nginx -y
+# Install Nginx if it not already installed
+if ! command -v nginx &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get -y install nginx
 fi
 
-# Create the necessary directories if they don't exist
-sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+# Create the folder /data/ if it doesn’t already exist
+# Create the folder /data/web_static/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/ if it doesn’t already exist
+# Create the folder /data/web_static/shared/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-# Create a HTML file for testing purposes
-sudo echo '
-<html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
+# Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
+echo "<html lang="en">
+  <head>
+	<meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="keywords" content="ALX, Web Server, AirBnB Clone, HTML, CSS, JavaScript">
         <meta name="author" content="Abdulqudus Oladega">
-        <title>Test Page</title>
-    </head>
+	<title>Hello World!</title>
+  </head>
+  <body>
+    <h1>This is a test page! 😁</h1>
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-    <body>
-        <header>
-            <h1>First Deployment! 😁</h1>
-        </header>
 
-    </body>
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-</html>
-' | sudo tee -a /data/web_static/releases/test/index.html
-
-# Create a symbolic link to the current release. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Set ownership of /data/ to the ubuntu user and group - recursive
+# Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update the Nginx configuration to serve content from the /data/web_static/current directory
-sudo sed -i '41i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-# Restart Nginx to apply the new configuration
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static).
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static {\n\talias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
